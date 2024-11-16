b7f22b652938f1839b68715029d5f1aeaa39c08d
Lev Tsvey <lev.tsvey@gmail.com>
2024-11-16 13:10:00 +0300

    add raw sprint api

---
+++ sprint_health/__init__.py
@@ -0,0 +1,15 @@
+from database import *
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class SprintHealth:
+    correct_order: int
+    good_planning: int
+    to_do_percentage: int
+    cancelled_percentage: int
+    backlog: int
+
+
+def get_sprint_health(sprint_id: int, time: datetime) -> SprintHealth:
+    pass
