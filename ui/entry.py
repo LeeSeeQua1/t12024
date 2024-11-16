225cd710d6fd34b0ca2c8c06712c4b0c5a1164d9
PrigaraRobert <409401@niuitmo.ru>
2024-11-16 18:25:33 +0300

    temp

--- ui/entry.py
+++ ui/entry.py
@@ -1,12 +1,74 @@
-from PySide6.QtWidgets import QMainWindow, QTabWidget
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QMainWindow, QTabWidget, QPushButton, QFileDialog, QVBoxLayout, QWidget, QHBoxLayout, \
+    QLabel, QLineEdit
+
+from sprint_health.sprint_health_api import get_spring_health
 
 
-class Window(QMainWindow):
+# from database import data_split
+
+
+class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Zoo Database')
-        self.tab_widget = QTabWidget(self)
-        self.setCentralWidget(self.tab_widget)
-        self.menu = self.menuBar()
-        self.file_menu = self.menu.addMenu('File')
-        self._controllers = []
+        self.l2 = QHBoxLayout(self)
+        l1 = QVBoxLayout()
+        self.l2.addLayout(l1)
+        for i in range(3):
+            btn = QPushButton('Set image')
+            btn.clicked.connect(lambda: self._choose_file(i))
+            l1.addWidget(btn)
+        self._files: list[str | None] = [None, None, None]
+        self._push_btn = QPushButton('Analyze')
+        self._push_btn.clicked.connect(self._confirm)
+        l1.addWidget(self._push_btn)
+        self.setLayout(self.l2)
+        self._graph_widget = None
+
+    def _choose_file(self, index: int):
+        dial = QFileDialog(self)
+        dial.setNameFilter("Csv (*.csv)")
+        if dial.exec():
+            path = dial.selectedFiles()
+            self._files[index] = path
+
+    def _confirm(self):
+        # if not all(self._files):
+        #     error_dialog = QtWidgets.QErrorMessage()
+        #     error_dialog.showMessage('You should clip all necessary tables')
+        #     return
+        # self.l2.addWidget(Graph(data_split(*self._files)))
+
+        if self._graph_widget is not None:
+            self._graph_widget.setParent(None)
+
+        self._graph_widget = Graph(10)
+        self.l2.addWidget(self._graph_widget)
+
+
+class Graph(QWidget):
+    def __init__(self, num_of_rows: int):
+        super().__init__()
+        self._num_of_rows = num_of_rows
+        self._text = QLineEdit("Text")
+        layout = QVBoxLayout()
+        layout.addWidget(self._text)
+        self.setLayout(layout)
+        btn = QPushButton("push")
+        btn.clicked.connect(lambda: self._get_sprint(self._text.text()))
+        layout.addWidget(btn)
+
+    def _get_sprint(self, sprint_id: str):
+
+        try:
+            sprint_id = int(sprint_id)
+        except ValueError:
+            pass
+        if isinstance(sprint_id, str) or sprint_id > self._num_of_rows:
+            error_dialog = QtWidgets.QMessageBox()
+            error_dialog.setText('You should clip all necessary tables')
+            error_dialog.exec()
+            return
+        ls = get_spring_health(sprint_id)
+        for i in ls:
+            print(i)
