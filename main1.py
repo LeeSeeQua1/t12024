7de0a80d3e93ea2de50e8286300cfaafeddb578f
denis <den.strumentov@gmail.com>
2024-11-16 23:20:47 +0300

    add m1

---
+++ main1.py
@@ -0,0 +1,8 @@
+from database import data_split
+
+
+data_split(
+    "./dataset/sprints-Table 1.csv",
+    "./dataset/data_for_spb_hakaton_entities1-Table 1.csv",
+    "./dataset/history-Table 1.csv"
+)
