7d94388da830e73edfdfba60a08cccf8ff0daf31
PrigaraRobert <409401@niuitmo.ru>
2024-11-17 12:40:47 +0300

    fixed size and file uploading

--- ui/entry.py
+++ ui/entry.py
@@ -3,6 +3,7 @@ from PySide6.QtCharts import QChart, QChartView, QBarCategoryAxis, QBarSeries, Q
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QMainWindow, QTabWidget, QPushButton, QFileDialog, QVBoxLayout, QWidget, QHBoxLayout, \
     QLabel, QLineEdit, QGridLayout, QSlider, QProgressBar
+import os
 
 from sprint_health.sprint_health_api import get_spring_health, StateFrame
 
@@ -17,23 +18,24 @@ class Window(QWidget):
         l1 = QVBoxLayout()
         self.l2.addLayout(l1)
 
-        tmp = ["Sprints", "Tasks", "History"]
-        btn = QPushButton(tmp[0])
-        btn.clicked.connect(lambda: self._choose_file(0))
-        l1.addWidget(btn)
-        btn = QPushButton(tmp[1])
-        btn.clicked.connect(lambda: self._choose_file(1))
-        l1.addWidget(btn)
-        btn = QPushButton(tmp[2])
-        btn.clicked.connect(lambda: self._choose_file(2))
-        l1.addWidget(btn)
+        self._table_names = ["Sprints", "Tasks", "History"]
+        btn1 = QPushButton(self._table_names[0])
+        btn1.clicked.connect(lambda: self._choose_file(0))
+        l1.addWidget(btn1)
+        btn2 = QPushButton(self._table_names[1])
+        btn2.clicked.connect(lambda: self._choose_file(1))
+        l1.addWidget(btn2)
+        btn3 = QPushButton(self._table_names[2])
+        btn3.clicked.connect(lambda: self._choose_file(2))
+        l1.addWidget(btn3)
+        self._btns = [btn1, btn2, btn3]
 
         self._files: list[str | None] = [None, None, None]
         self._push_btn = QPushButton('Analyze')
         self._push_btn.clicked.connect(self._confirm)
         l1.addWidget(self._push_btn)
         self._graph_widget = None
-        self.setGeometry(300, 150, 350, 300)
+        self.setGeometry(300, 150, 550, 300)
 
 #
         self.col_lay = None
@@ -47,10 +49,12 @@ class Window(QWidget):
         dial = QFileDialog(self)
         dial.setNameFilter("Csv (*.csv)")
         if dial.exec():
-            path = dial.selectedFiles()
-            self._files[index] = path[0]
+            path = dial.selectedFiles()[0]
+            self._files[index] = path
+            self._btns[index].setText(os.path.basename(path) + ' (' + self._table_names[index] + ')')
 
     def _confirm(self):
+        # todo
         # if not all(self._files):
         #     error_dialog = QtWidgets.QErrorMessage()
         #     error_dialog.showMessage('You should clip all necessary tables')
